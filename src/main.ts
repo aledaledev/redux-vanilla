e4fdaaab2ec53c31454e59736f299cbe96492f33
aledale <ale.salas271102@gmail.com>
2022-12-26 23:59:04 -0300

    playing with pure redux

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const display = document.getElementById('click-quantity')
+
+const buttonIncrement = document.getElementById('increment')
+
+const buttonDecrement = document.getElementById('decrement')
